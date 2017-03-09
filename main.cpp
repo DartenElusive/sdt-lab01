26c9ec90321f798d4be7d4d5a2694834e8cb2c73
Дмитрий Камендровский <ecliptor110@mail.ru>
2017-03-09 16:33:43 +0300

    изменение параметров

--- main.cpp
+++ main.cpp
@@ -6,5 +6,9 @@ main() {
     int a,b;
     cout<<"Enter A and B";
     cin>>a>>b;
+    cout<<"A+B="<<a+b<<"\n"
+            <<"A/2="<<a/2<<"\n"
+            <<"A-B="<<a-b<<"\n"
+            <<"A/B="<<a/b<<"\n";
     return 0;
 }
